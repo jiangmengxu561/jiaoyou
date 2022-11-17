6dea6700a5349b2c96f23f0bfa34dd187bcb42a0
jiangmengxu561 <17610521368@163.com>
2022-11-17 10:59:43 +0800

    1

--- application/config.php
+++ application/config.php
@@ -253,7 +253,7 @@ return [
         // 驱动方式
         'type'     => 'Mysql',
         // 缓存前缀
-        'key'      => 'i3d6o32wo8fvs1fvdpwens',
+        'key'      => 'ZystCxOkBSvwWVcpUgPMm2Qhl45eIFnb',
         // 加密方式
         'hashalgo' => 'ripemd160',
         // 缓存有效期 0表示永久缓存
